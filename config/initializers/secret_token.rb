39430f9650edd6c738c241251f3a3ff6cc43951b
Jesus <elfiel13@gmail.com>
2012-08-12 14:08:46 -0500

    Renovando - Gem Twitter-bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jsnetsolution::Application.config.secret_token = '512a9a35cd625d0dc6461d8bed9ffe43f19a3b1f1c7ca40609851009ceb8e59060ecefce2d25feed8dd51dc543b6c4dd931408501c01767f9de6beece6280adf'
