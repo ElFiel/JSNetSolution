7e6c3e7c7514c7657dffde24f7b69700ac35c37f
Jesus <elfiel13@gmail.com>
2012-08-21 17:56:32 -0500

    Barra superior - Valida contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,6 @@
 class Contact < ActiveRecord::Base
   attr_accessible :content, :email, :name, :otros, :theme
+
+  validates_presence_of :content, :email, :name, :theme
+  validates :email, :email => true, :presence => true
 end
