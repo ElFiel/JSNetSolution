ed1df548a66a26fac77f4829d6d4cba9fa691c72
Jesus <elfiel13@gmail.com>
2012-08-22 05:48:25 -0500

    Quiet assets - contact responsive

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,8 +34,6 @@ Jsnetsolution::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
-
-
   
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
