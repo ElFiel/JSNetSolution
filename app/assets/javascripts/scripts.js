aced5a4f98d59decae3103a2da90f6437fd34e8d
Jesus <elfiel13@gmail.com>
2012-09-20 02:55:12 -0500

    Cambiando Carousel, Home a haml, ancho para pantalla grande de publi2

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,8 +1,6 @@
 $(function() {
-  $('#ei-slider').eislideshow({
-	  animation			: 'center',
-	  autoplay			: true,
-	  slideshow_interval	: 3000,
-	  titlesFactor		: 0
-  });
+  $('.carousel').carousel(function(){
+      interval: 2000;
+    }
+  );
 });
